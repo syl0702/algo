be09690c59ac41042bc79504287f02ca780d6c51
syl0702 <navyblue794@gmail.com>
2023-07-30 19:39:35 +0900

    new method

--- swea/6242_for6/sol2.py
+++ swea/6242_for6/sol2.py
@@ -1,21 +1,6 @@
 blood_list = ['A','A','A','O','B','B','O','AB','AB','O']
-bnum_list = []
-for bnum in blood_list:
-    if bnum == 'A':
-        bnum.count('A')
-        bnum_list.append(bnum.count('A'))
-    
-    elif bnum == 'O':
-        bnum.count('O')
-        bnum_list.append(bnum.count('O'))
+bnum_dict = {}
+for blood in ['A', 'B', 'O', 'AB']:
+    bnum_dict[blood] = blood_list.count(blood)
 
-    elif bnum == 'B':
-        bnum.count('B')
-        bnum_list.append(bnum.count('B'))
-
-    else:
-        bnum.count('AB')
-        bnum.count('AB')
-        bnum_list.append(bnum.count('AB'))
-
-print (bnum_list)
+print (bnum_dict)
