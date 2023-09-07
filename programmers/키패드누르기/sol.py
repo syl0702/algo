327cbb481c8139a1d58e6d3b7fa982a00801c7f2
syl0702 <navyblue794@gmail.com>
2023-09-07 10:59:08 +0900

    키패드누르기

--- programmers/키패드누르기/sol.py
+++ programmers/키패드누르기/sol.py
@@ -1,18 +1,40 @@
 def solution(numbers, hand):
-    answer = ''
+    
     num = {1: [0,0], 2: [0,1], 3: [0,2], 4: [1,0], 
            5: [1,1], 6: [1,2], 7: [2,0], 8:[2,1], 9: [2,2], '*': [3,0], 0: [3,1], '#': [3,2]}
-    left = []
-    right = []
-    for n in numbers:
-        if n in [1, 4, 7]:
+    answer = ''
+    r = num['#']
+    l = num['*']
+    
+    for i in numbers:
+        now = num[i]
+
+        if i in [1, 4, 7]:
+            l = now
             answer += 'L'
-            left.append(n)
-        elif n in [3, 6, 9]:
+        elif i in [3, 6, 9]:
+            r = now
             answer += 'R'
-            right.append(n)
         else:
-            answer += 'N'
+            l_d = 0
+            r_d = 0
+
+            for x, y, z in zip(l, r, now):
+                l_d += abs(x-z)
+                r_d += abs(y-z)
+            if l_d < r_d:
+                l = now
+                answer += 'L'
+            elif l_d > r_d:
+                r = now
+                answer +='R'
+            else:
+                if hand == 'right':
+                    r = now
+                    answer += 'R'
+                else:
+                    l = now
+                    answer += 'L'
             
     return answer
 
