2ea6d750642212bd682115fa79c004c3f8657889
syl0702 <navyblue794@gmail.com>
2023-09-09 02:40:38 +0900

    update

--- programmers/신규아이디추천/sol.py
+++ programmers/신규아이디추천/sol.py
@@ -1,23 +1,41 @@
 import re
 def solution(new_id):
     answer = ''
-    result = ''
-    new = new_id.lower()
-    for n in new:
+    # result = ''
+    new_id = new_id.lower()
+    
+    for n in new_id:
         if n.isalnum() or n in '-_.':
-            result += n
-    for a in range(1, len(result)):
-        if result[a-1] != result[a]:
-            answer += result[a]
-    index = answer.find('.')
-    if index == -1:
-        answer[:-1]
-    elif index == 0:
-        answer[0:]
+            answer += n
+    # for a in range(1, len(result)):
+    #     if result[a-1] != result[a]:
+            
+    #         answer += result[a]
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+            
+    
+    if answer[0] == '.' and len(answer)>1:
+        answer = answer[1:]
+    else:
+        answer
+    
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    else: answer
 
-                
-                
+    if answer == '':
+        answer = 'a'
+    else:
+        answer
 
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    
+    if len(answer) <= 3:
+        answer = answer + answer[-1]*(3-len(answer))
 
     return answer
 
