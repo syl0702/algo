47e3129945667ebd062666576d47a2fef98fbf4a
syl0702 <navyblue794@gmail.com>
2023-09-10 23:48:40 +0900

    review

---
+++ swea/5097_회전/sol_r.py
@@ -0,0 +1,9 @@
+import sys
+from pprint import pprint
+sys.stdin = open('input.txt')
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = list(map(int, input().split()))
+    numbers = list(map(int, input().split()))
+    print(numbers[M % N]) 
