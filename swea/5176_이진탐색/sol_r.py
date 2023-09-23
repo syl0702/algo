d00f6828a0c70370067628ca2fb7c82d44bf6e76
syl0702 <navyblue794@gmail.com>
2023-09-24 00:07:16 +0900

    update

--- swea/5176_이진탐색/sol_r.py
+++ swea/5176_이진탐색/sol_r.py
@@ -2,5 +2,25 @@ import sys
 sys.stdin = open('input.txt')
 T = int(input())
 
+def intree(idx):
+    global count
+    if idx <= N:
+        intree(idx*2)
+
+        tree[idx] = count
+        count += 1
+
+        intree(idx*2 + 1)
+
+
+
+
 for tc in range(1, T+1):
     N = int(input())
+    tree = [0]*(N+1)
+
+    count = 1
+
+    intree(1)
+    print(tree[1])
+    print(tree[N//2])
