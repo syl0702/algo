cf27b24d9696ef731de58891d4e488d98be6c0f2
syl0702 <navyblue794@gmail.com>
2023-10-01 19:37:56 +0900

    이진힙reviewing

---
+++ swea/5177_이진힙/sol_r.py
@@ -0,0 +1,9 @@
+import sys
+from pprint import pprint
+sys.stdin = open('input.txt')
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    numbers = list(map(int, input().split()))
+    
