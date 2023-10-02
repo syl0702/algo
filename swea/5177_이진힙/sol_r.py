d16056ff3c020d8067b58d8f43963d370b787974
syl0702 <navyblue794@gmail.com>
2023-10-02 10:24:29 +0900

    review

--- swea/5177_이진힙/sol_r.py
+++ swea/5177_이진힙/sol_r.py
@@ -7,3 +7,29 @@ for tc in range(1, T+1):
     N = int(input())
     numbers = list(map(int, input().split()))
     
+    heap = [0]*(N+1)
+    n = 0
+
+    for i in range(N):
+        n += 1
+        heap[n] = numbers[i]
+        child = n
+        parent = child // 2
+
+        while parent and heap[parent] > heap[child]:
+            heap[parent], heap[child] = heap[child], heap[parent]
+
+            child = parent
+            parent = child // 2
+    
+    print(heap)
+
+    node = N // 2
+    total = 0
+
+    while node:
+        total += heap[node]
+        node //= 2
+    
+    print(total)
+    
