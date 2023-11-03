99760876ae03937122a61d084a54407fddaadf48
syl0702 <navyblue794@gmail.com>
2023-11-03 22:26:12 +0900

    옹알이1

---
+++ programmers/옹알이1/sol.py
@@ -0,0 +1,7 @@
+def solution(babbling):
+    answer = 0
+    
+    return answer
+
+print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
+print(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
