cd5a1c9b8a5205fd5cf53a044cdb95d9650ae05e
syl0702 <navyblue794@gmail.com>
2023-11-03 22:53:57 +0900

    옹알이1 sol

--- programmers/옹알이1/sol.py
+++ programmers/옹알이1/sol.py
@@ -1,6 +1,16 @@
 def solution(babbling):
     answer = 0
-    
+    for babble in babbling:
+        result = 0
+        word = ''
+        for b in babble:
+            word += b
+            if word in ["aya", "ye", "woo", "ma"]:
+               word = ''
+               result += 1
+        if len(word) == 0 and result > 0:
+            answer += 1
+
     return answer
 
 print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
