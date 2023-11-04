0fc34216f8617f325d0f104822421a5ec1cd0e7f
syl0702 <navyblue794@gmail.com>
2023-11-04 22:28:31 +0900

    옹알이2 헤매는 중

--- programmers/옹알이2/sol.py
+++ programmers/옹알이2/sol.py
@@ -1,15 +1,40 @@
+import re
 def solution(babbling):
     answer = 0
     for babble in babbling:
         result = 0
-        word = ''
-        for b in babble:
-            word += b
-            if word in ["aya", "ye", "woo", "ma"]:
-               word = ''
-               result += 1
-        if len(word) == 0 and result > 0:
-            answer += 1
+        if "aya" in babble:
+            babble = babble.replace("aya", "*")
+            
+            # babble_c = babble.count("*")
+            # if babble_c == 1:
+            #     result += 1
+        elif "ye" in babble:
+            babble = babble.replace("ye","%")
+            # babble_c = babble.count("%")
+            # if babble_c == 1:
+            #     result += 1
+        
+        elif "woo" in babble:
+            babble = babble.replace("woo", "#")
+            # babble_c = babble.count("#")
+            # if babble_c == 1:
+            #     result += 1
+        
+        elif "ma" in babble:
+            babble = babble.replace("ma", "@")
+            # babble_c = babble.count("@")
+            # if babble_c == 1:
+            #     result += 1
+        
+
+        # for b in babble:
+        #     word += b
+        #     if word in ["aya", "ye", "woo", "ma"]:
+        #        word = ''
+        #        result += 1
+        # if len(word) == 0 and result > 0:
+        #     answer += 1
 
     return answer
 
