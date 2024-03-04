4f5aad10a9671d7a9f70dab4be0c8a0396ae681c
syl0702 <navyblue794@gmail.com>
2024-03-04 22:35:56 +0900

    최댓값과최솟값 풀기

--- programmers/최댓값과최솟값/sol.py
+++ programmers/최댓값과최솟값/sol.py
@@ -1,3 +1,10 @@
 def solution(s):
+    s1 = int(s)
+    temp = s1.sort()
+    print(temp)
     answer = ''
     return answer
+
+print(solution("1 2 3 4"))
+print(solution("-1 -2 -3 -4"))
+print(solution("-1 -1"))
