24ce3038e807ec76cce8178dd48884da84d7e870
syl0702 <navyblue794@gmail.com>
2024-03-08 22:26:31 +0900

    solved

--- programmers/최댓값과최솟값/sol.py
+++ programmers/최댓값과최솟값/sol.py
@@ -1,7 +1,13 @@
 def solution(s):
     s1 = list(map(int, s.split()))
-    print(s1)
-    answer = ''
+    # print(s1)
+    temp = []
+    temp.append(min(s1))
+    temp.append(max(s1))
+    # print(temp)
+    answer = ' '.join(map(str, temp))
+    # print(result)
+    # answer = ''
     return answer
 
 print(solution("1 2 3 4"))
