9f78745664bf4f729bc0a7fe563f792eda0467fe
syl0702 <navyblue794@gmail.com>
2024-03-27 00:49:56 +0900

    가장큰수 solved

---
+++ programmers/가장큰수/sol.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+def solution(numbers):
+    answer = ''
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    # print(numbers)
+    answer = str(int(''.join(numbers)))
+    return answer
+
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
