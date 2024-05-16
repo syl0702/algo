880d8858548b309d498933f0c4dddf76a901f0e1
syl0702 <navyblue794@gmail.com>
2024-05-16 23:32:58 +0900

    def

--- programmers/이진변환반복하기/sol.py
+++ programmers/이진변환반복하기/sol.py
@@ -5,15 +5,16 @@ def solution(s):
         s11 = s.replace('0')
         s22 = bin(int(s11))
         s = str(s22)
+        temp += 1
         return
     
-    s1= s.replace('0', '')
-    s2 = int(s1)
-    s3 = str(bin(s2))
-    if '0' in s3:
-        s3 = s3.replace('0', '')
+    # s1= s.replace('0', '')
+    # s2 = int(s1)
+    # s3 = str(bin(s2))
+    if '0' in s:
+        change(s)
         
-    print(s3)
+    # print(s3)
     return answer
 
 print(solution("110010101001"))
