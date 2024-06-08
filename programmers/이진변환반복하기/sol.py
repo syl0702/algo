9f40c94a6a156be2c99b9a8339a5ff54a9c25800
syl0702 <navyblue794@gmail.com>
2024-06-08 22:17:38 +0900

    for? if?

--- programmers/이진변환반복하기/sol.py
+++ programmers/이진변환반복하기/sol.py
@@ -1,19 +1,21 @@
 def solution(s):
     answer = []
     def change(s):
-        temp = 0
+        # temp = 0
         s11 = s.replace('0')
         s22 = bin(int(s11))
         s = str(s22)
-        temp += 1
+        # temp += 1
         # return
     
     # s1= s.replace('0', '')
     # s2 = int(s1)
     # s3 = str(bin(s2))
+    temp = 0
     for i in len(s):
         if i == '0':
             change(s)
+            temp += 1
         else:
             pass
         
