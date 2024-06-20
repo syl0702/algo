3e6e4e1d832ca4870774fd82b5b0662344094998
syl0702 <navyblue794@gmail.com>
2024-06-20 11:13:57 +0900

    misunderstood

--- programmers/이진변환반복하기/sol.py
+++ programmers/이진변환반복하기/sol.py
@@ -1,31 +1,21 @@
 def solution(s):
     answer = []
-    def change(s):
-        # temp = 0
-        s11 = s.replace('0', '')
-        s22 = bin(int(s11))
-        s33 = int(s22, 2)
-        print(s33)
-        return s33
-        # temp += 1
-        # return
+    # print(int(len(s)))
+    def proc(s):
+        s1 = s.replace("0", "")
+        s2 = bin(len(s1))
+        s = s2
+        return s
     
-    # s1= s.replace('0', '')
-    # s2 = int(s1)
-    # s3 = str(bin(s2))
     temp = 0
-    for i in range(len(s)):
-        if len(s) != 1 & s[i] == '0':
-            change(s)
-            temp += 1
-        
-        elif len(s) ==1 & s[i] == 's':
-            answer.append(temp)
-        else:
-            pass
-        
-    answer.append(temp)
-    # print(s3)
+    if int(s) != 1:
+        proc(s)
+        temp += 1
+        print(s)
+    else:
+        temp += 1
+        answer.append(temp)
+
     return answer
 
 print(solution("110010101001"))
