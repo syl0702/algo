7f03cf840e7aa1631b0f651ee1307339d9063e2c
syl0702 <navyblue794@gmail.com>
2024-06-20 11:25:46 +0900

    solved

--- programmers/이진변환반복하기/sol.py
+++ programmers/이진변환반복하기/sol.py
@@ -1,21 +1,17 @@
 def solution(s):
     answer = []
+    bcnt = 0
+    zerocnt = 0
     # print(int(len(s)))
-    def proc(s):
-        s1 = s.replace("0", "")
-        s2 = bin(len(s1))
-        s = s2
-        return s
-    
-    temp = 0
-    if int(s) != 1:
-        proc(s)
-        temp += 1
-        print(s)
-    else:
-        temp += 1
-        answer.append(temp)
+    while True:
+        if s == '1':
+            break
+        zerocnt += s.count('0')
+        s = s.replace('0', '')
+        s = bin(len(s))[2:]
+        bcnt += 1
 
+    answer = [bcnt, zerocnt]
     return answer
 
 print(solution("110010101001"))
