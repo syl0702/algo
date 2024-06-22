83cad5eb367fb0c95dc94a24373c262efb23f678
syl0702 <navyblue794@gmail.com>
2024-06-22 15:14:37 +0900

    2nd

--- programmers/숫자의표현/sol.py
+++ programmers/숫자의표현/sol.py
@@ -1,16 +1,19 @@
 def solution(n):
     answer = 0
     sum = 0
-    for i in range(1, n+1):
-        # print(i)
-        sum += i
-        if sum < n:
-            i += 1
-        elif sum > n:
-            break
-        else:
-            answer += 1
-            continue
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            # print(i)
+            sum += i
+            if sum < n:
+                i += 1
+            elif sum > n:
+                k+=1
+                break
+            else:
+                answer += 1
+                k+=1
+                continue
     return answer
 
 print(solution(15))
