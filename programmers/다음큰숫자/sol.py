84d992659d9e118937e71184b7062280a457ff2e
syl0702 <navyblue794@gmail.com>
2024-06-22 23:18:36 +0900

    solved!

--- programmers/다음큰숫자/sol.py
+++ programmers/다음큰숫자/sol.py
@@ -3,11 +3,15 @@ def solution(n):
     binn = str(bin(n)[2:])
     strbin = binn.count("1")
     # print(strbin)
-    for i in range(n):
+    for i in range(1, n):
         i1 = n+i
         i2 = str(bin(i1)[2:])
         if i2.count("1") == strbin:
             answer = i1
+            break
+        else:
+            i+= 1
+            continue
     return answer
 
 print(solution(78))
