73019e87e223fe75bf70d9d4ca7023b6905b4509
syl0702 <navyblue794@gmail.com>
2024-06-22 23:08:32 +0900

    why incorrect answer coming out?

--- programmers/다음큰숫자/sol.py
+++ programmers/다음큰숫자/sol.py
@@ -2,8 +2,12 @@ def solution(n):
     answer = 0
     binn = str(bin(n)[2:])
     strbin = binn.count("1")
-    print(strbin)
-    
+    # print(strbin)
+    for i in range(n):
+        i1 = n+i
+        i2 = str(bin(i1)[2:])
+        if i2.count("1") == strbin:
+            answer = i1
     return answer
 
 print(solution(78))
