13099de633ad6ced876e8f88d68490b71bd3ee19
syl0702 <navyblue794@gmail.com>
2024-06-23 11:04:04 +0900

    runtime error

--- programmers/피보나치수/sol.py
+++ programmers/피보나치수/sol.py
@@ -1,5 +1,13 @@
 def solution(n):
     answer = 0
+    if n == 1 or n ==2:
+        answer+= 1
+    elif n== 0:
+        answer = 0
+
+    else:
+        answer += solution(n-1) + solution(n-2) % 1234567
+
     return answer
 
 print(solution(3))
