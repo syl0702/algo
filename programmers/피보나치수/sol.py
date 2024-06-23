62ab816792afa6969476d4ef338883d510f327db
syl0702 <navyblue794@gmail.com>
2024-06-23 15:08:52 +0900

    solved

--- programmers/피보나치수/sol.py
+++ programmers/피보나치수/sol.py
@@ -1,13 +1,23 @@
 def solution(n):
     answer = 0
-    if n == 1 or n ==2:
-        answer+= 1
-    elif n== 0:
-        answer = 0
+    # time error
+    # if n == 1 or n ==2:
+    #     answer+= 1
+    # elif n== 0:
+    #     answer = 0
 
-    else:
-        answer += solution(n-1) + solution(n-2) % 1234567
+    # else:
+    #     answer += (solution(n-1) + solution(n-2)) % 1234567
 
+
+    if n < 2:
+        answer += n
+    cache = [0 for _ in range(n+1)]
+    cache[1] = 1
+
+    for i in range(2, n+1):
+        cache[i] = cache[i-1] + cache[i-2]
+        answer += cache[n] % 1234567
     return answer
 
 print(solution(3))
