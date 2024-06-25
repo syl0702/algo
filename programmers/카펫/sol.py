e0caf937a2cbe07063cade060efe73befb46e08e
syl0702 <navyblue794@gmail.com>
2024-06-25 19:10:48 +0900

    failed cases

--- programmers/카펫/sol.py
+++ programmers/카펫/sol.py
@@ -1,3 +1,14 @@
 def solution(brown, yellow):
     answer = []
+    sums = brown + yellow
+    for i in range(1, sums):
+        if sums % i == 0:
+            pairs = sums // i
+            if abs(pairs-i) <= 2:
+                answer = [i, pairs]
+                answer.sort(reverse=True)
     return answer
+
+print(solution(10, 2))
+print(solution(8, 1))
+print(solution(24, 24))
