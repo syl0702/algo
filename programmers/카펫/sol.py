bfc80af3e01e8c624390aa78b50e9e74db2176d1
syl0702 <navyblue794@gmail.com>
2024-06-25 20:01:20 +0900

    76.9%

--- programmers/카펫/sol.py
+++ programmers/카펫/sol.py
@@ -2,22 +2,14 @@ def solution(brown, yellow):
     answer = []
     mins = float('inf')
     sums = brown + yellow
-    for i in range(1, sums):
+    for i in range(1, sums//2):
         if sums % i == 0:
             pairs = sums // i
             diff = abs(pairs-i)
             if diff < mins:
                 mins = diff
                 answer = [i, pairs]
-            
     answer.sort(reverse=True)        
-
-            # answer = mins
-            # answer.sort(reverse=True)
-            # if abs(pairs-i) <= 2:
-            #     answer = [i, pairs]
-            #     answer.sort(reverse=True)
-            
     return answer
 
 print(solution(10, 2))
