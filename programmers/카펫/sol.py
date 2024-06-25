42a6360ffef09f0a59a4a8cc24dc91cf8a4211b7
syl0702 <navyblue794@gmail.com>
2024-06-25 21:55:13 +0900

    solved but review needed

--- programmers/카펫/sol.py
+++ programmers/카펫/sol.py
@@ -1,16 +1,27 @@
 def solution(brown, yellow):
     answer = []
-    mins = float('inf')
+    # mins = float('inf')
+    # sums = brown + yellow
+    # for i in range(1, sums//2):
+    #     if sums % i == 0:
+    #         pairs = sums // i
+    #         diff = abs(pairs-i)
+    #         if diff < mins:
+    #             mins = diff
+    #             answer = [i, pairs]
+    # answer.sort(reverse=True)
+    # 76.9% 정답률이 최대
+
+    # 2nd solution
     sums = brown + yellow
-    for i in range(1, sums//2):
-        if sums % i == 0:
-            pairs = sums // i
-            diff = abs(pairs-i)
-            if diff < mins:
-                mins = diff
-                answer = [i, pairs]
-    answer.sort(reverse=True)        
+    for bh in range(1, brown//2+1):
+        bw = (brown - 2*bh+4) //2
+        yw, yh = bw-2, bh-2
+        if yellow == yw * yh and sums == bw*bh:
+            answer = [bh, bw]
+    
     return answer
+
 
 print(solution(10, 2))
 print(solution(8, 1))
