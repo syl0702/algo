238270f7d4c2350f7f04116dd0ff928df8be7d00
syl0702 <navyblue794@gmail.com>
2024-06-27 13:51:48 +0900

    difficult..

--- programmers/구명보트/sol.py
+++ programmers/구명보트/sol.py
@@ -1,5 +1,20 @@
 def solution(people, limit):
     answer = 0
+    people.sort()
+    # print(people)
+    # for p in range(len(people)-1):
+    #     print(people[p])
+    if people[0] + people[1] <= limit:
+        answer += 1
+        for p in range(1, len(people)-2):
+            if people[p] + people[p+1] <= limit:
+                answer += 1
+                continue
+            else:
+                answer += len(people) - (p+1)
+            
+    else:
+        answer += len(people)
     return answer
 
 print(solution([70, 50, 80, 50], 100))
