5ae83d2171b35ddd573f810098fc820bbaef02e6
syl0702 <navyblue794@gmail.com>
2024-06-27 17:10:10 +0900

    confused

--- programmers/구명보트/sol.py
+++ programmers/구명보트/sol.py
@@ -1,20 +1,41 @@
 def solution(people, limit):
     answer = 0
     people.sort()
+    i = 0 # 시작 인덱스
     # print(people)
     # for p in range(len(people)-1):
     #     print(people[p])
-    if people[0] + people[1] <= limit:
-        answer += 1
-        for p in range(1, len(people)-2):
-            if people[p] + people[p+1] <= limit:
-                answer += 1
-                continue
-            else:
-                answer += len(people) - (p+1)
+    # if people[0] + people[1] <= limit:
+    #     answer += 1
+    #     for p in range(2, len(people)):
+    #         if people[p] + people[p+1] <= limit:
+    #             answer += 1
+    #             continue
+    #         else:
+    #             answer += len(people) - (p+1)
+    #             break
             
-    else:
-        answer += len(people)
+    # else:
+    #     answer += len(people)
+    while i < len(people):
+        if i < len(people)-1 and people[i] + people[i+1] <= limit:
+            answer += 1
+            i += 2
+        else:
+            answer += 1
+            i += 1
+
+    # for p in range(len(people)-1):
+    #     if p >= len(people):
+    #         break
+    #     if p < len(people)-1 and people[p] + people[p+1] <= limit:
+    #         answer+=1
+    #         p+=2
+            
+    #     else:
+    #         answer += 1
+    #         p+=1
+            
     return answer
 
 print(solution([70, 50, 80, 50], 100))
