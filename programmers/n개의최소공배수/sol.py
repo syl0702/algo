1948390199938edfe6fd63141beb9093c0e30c01
syl0702 <navyblue794@gmail.com>
2024-06-28 22:29:55 +0900

    wrong answer

--- programmers/n개의최소공배수/sol.py
+++ programmers/n개의최소공배수/sol.py
@@ -4,15 +4,16 @@ def lcm(a,b):
     return a*b / math.gcd(a,b)
 def solution(arr):
     answer = 0
-    arr = deque(arr)
+    # arr = deque(arr)
     temp = []
-    # for a in range(len(arr)-2):
-    #     arr[a] = lcm(arr[a], arr[a+1])
-    #     a+=1
-    while len(arr) > 1:
-        temp.append(lcm(arr[0], arr[-1]))
-        arr.pop()
-        arr.popleft()
+    for a in range(len(arr)-2):
+        arr[a] = lcm(arr[a], arr[a+1])
+        a+=1
+    answer += arr[a]
+    # while len(arr) > 1:
+    #     temp.append(lcm(arr[0], arr[-1]))
+    #     arr.pop()
+    #     arr.popleft()
 
     return answer
 
