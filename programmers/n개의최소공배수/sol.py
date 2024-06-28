fe249fd9184852f3fe473e889bc23f670a91a872
syl0702 <navyblue794@gmail.com>
2024-06-28 22:17:20 +0900

    complex

--- programmers/n개의최소공배수/sol.py
+++ programmers/n개의최소공배수/sol.py
@@ -1,5 +1,19 @@
+import math
+from collections import deque
+def lcm(a,b):
+    return a*b / math.gcd(a,b)
 def solution(arr):
     answer = 0
+    arr = deque(arr)
+    temp = []
+    # for a in range(len(arr)-2):
+    #     arr[a] = lcm(arr[a], arr[a+1])
+    #     a+=1
+    while len(arr) > 1:
+        temp.append(lcm(arr[0], arr[-1]))
+        arr.pop()
+        arr.popleft()
+
     return answer
 
 print(solution([2, 6, 8, 14]))
