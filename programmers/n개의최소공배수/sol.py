03c6201c74a42a0c9ed1317d9b1518b5e00d4dbe
syl0702 <navyblue794@gmail.com>
2024-07-01 23:02:52 +0900

    still confusing

--- programmers/n개의최소공배수/sol.py
+++ programmers/n개의최소공배수/sol.py
@@ -4,17 +4,9 @@ def lcm(a,b):
     return a*b / math.gcd(a,b)
 def solution(arr):
     answer = 0
-    # arr = deque(arr)
-    temp = []
-    temp.append(lcm(arr[0], arr[1]))
-    for a in range(len(arr)-2):
-        arr[a] = lcm(arr[a], arr[a+1])
-        a+=1
-    answer += arr[a]
-    # while len(arr) > 1:
-    #     temp.append(lcm(arr[0], arr[-1]))
-    #     arr.pop()
-    #     arr.popleft()
+    for i in range(arr):
+        arr[i+1] = lcm(arr[i], arr[i+1])
+        i+=2
 
     return answer
 
