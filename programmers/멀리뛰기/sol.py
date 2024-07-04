98ae5ebfa8a3f3b05b454c5090fc5c10e3b386ad
syl0702 <navyblue794@gmail.com>
2024-07-04 21:36:51 +0900

    first trial

--- programmers/멀리뛰기/sol.py
+++ programmers/멀리뛰기/sol.py
@@ -1,5 +1,14 @@
 def solution(n):
     answer = 0
+    temp = []
+    if n > 2:
+        temp.append(1)
+        n = n-1
+    elif n == 2:
+        temp.append(2)
+        n = n-2
+    else:
+        temp.append(1)
     return answer
 
 print(solution(4))
