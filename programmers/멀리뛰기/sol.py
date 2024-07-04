e56ea7029de55d17b3671f290b6897114b22c6a3
syl0702 <navyblue794@gmail.com>
2024-07-04 22:10:05 +0900

    confused

--- programmers/멀리뛰기/sol.py
+++ programmers/멀리뛰기/sol.py
@@ -1,14 +1,17 @@
 def solution(n):
     answer = 0
     temp = []
-    if n > 2:
-        temp.append(1)
-        n = n-1
-    elif n == 2:
-        temp.append(2)
-        n = n-2
-    else:
-        temp.append(1)
+    while n > 0:
+        if n > 2:
+            temp.append(2)
+            n = n-2
+        elif n == 2:
+            temp.append(2)
+            n = n-2
+        else:
+            temp.append(1)
+            n = n - 1
+    print(temp)
     return answer
 
 print(solution(4))
