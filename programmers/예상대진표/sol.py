1a2a99fcb71c72f7d50a7d7221eb3806b36718fd
syl0702 <navyblue794@gmail.com>
2024-07-04 19:45:03 +0900

    review needed

--- programmers/예상대진표/sol.py
+++ programmers/예상대진표/sol.py
@@ -1,10 +1,9 @@
 def solution(n,a,b):
     answer = 0
-    temp = []
-    for i in range(1, n):
-        if i % 2 == 0:
-            temp.append(i-1)
-    print(temp)
+    while a != b:
+        a = (a+1)//2
+        b = (b+1)//2
+        answer += 1
     
 
     return answer
