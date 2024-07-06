55d03678914d544adc5571ab9f62f6c72d08e939
syl0702 <navyblue794@gmail.com>
2024-07-06 23:43:58 +0900

    studying

--- programmers/멀리뛰기/sol.py
+++ programmers/멀리뛰기/sol.py
@@ -2,17 +2,19 @@ def solution(n):
     answer = 0
     temp = []
     dp = [[0]*(n+1) for _ in range(n+1)]
-    while n > 0:
-        if n > 2:
-            temp.append(2)
-            n = n-2
-        elif n == 2:
-            temp.append(2)
-            n = n-2
-        else:
-            temp.append(1)
-            n = n - 1
-    print(temp)
+    # 합이 같은 부분집합?
+
+    # while n > 0:
+    #     if n > 2:
+    #         temp.append(2)
+    #         n = n-2
+    #     elif n == 2:
+    #         temp.append(2)
+    #         n = n-2
+    #     else:
+    #         temp.append(1)
+    #         n = n - 1
+    # print(temp)
     return answer
 
 print(solution(4))
