caa724b2bc087a4d9a3f84f1906bd0897df6f4eb
syl0702 <navyblue794@gmail.com>
2024-07-09 22:13:23 +0900

    still confused

--- programmers/멀리뛰기/sol.py
+++ programmers/멀리뛰기/sol.py
@@ -1,19 +1,19 @@
-def get_case_count(a, b):
-    if a == 0:
-        return 1
-    if a < b:
-        b = a
+# def get_case_count(a, b):
+#     if a == 0:
+#         return 1
+#     if a < b:
+#         b = a
     
-    sum = 0
+#     sum = 0
 
-    while b > 0:
-        sum += get_case_count(a - b, b)
-        b -= 1
-    return sum + 1
+#     while b > 0:
+#         sum += get_case_count(a - b, b)
+#         b -= 1
+#     return sum + 1
 
 def solution(n):
     answer = 0
-    return get_case_count(n, n-1)
+    # 피보나치????
     # 합이 같은 부분집합?
 
     # while n > 0:
@@ -28,7 +28,7 @@ def solution(n):
     #         n = n - 1
     # print(temp)
 
-    # return answer
+    return answer
 
 print(solution(4))
 print(solution(3))
