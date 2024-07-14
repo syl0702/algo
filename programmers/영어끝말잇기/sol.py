f88fe823a6ede8db6934cda6f4a11a1577b79d05
syl0702 <navyblue794@gmail.com>
2024-07-14 22:16:26 +0900

    analyzing

--- programmers/영어끝말잇기/sol.py
+++ programmers/영어끝말잇기/sol.py
@@ -5,3 +5,7 @@ def solution(n, words):
     print('Hello Python')
 
     return answer
+
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
