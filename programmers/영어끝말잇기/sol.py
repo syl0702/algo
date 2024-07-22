2ac1ec7e4db79ddfdd10b7dcb9abe144f1fb3aee
syl0702 <navyblue794@gmail.com>
2024-07-22 15:52:07 +0900

    solved but review needed

--- programmers/영어끝말잇기/sol.py
+++ programmers/영어끝말잇기/sol.py
@@ -1,28 +1,20 @@
 def solution(n, words):
     answer = []
-    temp = set() # 이미 사용된 단어 추적.
-    temp.add(words[0])
-    for j in range(len(words)-1):
-        if j > 0:
-            last_char = words[j][-1]
-            first_char = words[j+1][0]
+    temp = [] # 이미 사용된 단어 추적.
+    temp.append(words[0])
+    for j in range(1, len(words)):
+        last_char = words[j-1][-1]
+        first_char = words[j][0]
+
+        if last_char == first_char and words[j] not in temp:
+            temp.append(words[j])
+
+        else:
+            return [(j%n)+1, (j//n)+1]
+
         
-            if last_char != first_char:
-                answer = [(j % n)+1, (j//n)+1]
-                break
 
-    # 단어 중복 체크
-        if words[j] in temp:
-            answer = [(j % n)+1, (j//n)+1]
-            break
-
-
-        temp.add(words[j])
-        
-    else:
-        answer = [0, 0]
-
-    return answer
+    return [0, 0]
 
 print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
 print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
