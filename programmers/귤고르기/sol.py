6252191f57c20ca58f27bae31e3112bca0c73b51
syl0702 <navyblue794@gmail.com>
2024-07-25 22:30:07 +0900

    example analysis

--- programmers/귤고르기/sol.py
+++ programmers/귤고르기/sol.py
@@ -1,3 +1,7 @@
 def solution(k, tangerine):
     answer = 0
     return answer
+
+print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
+print(solution(4, [1, 3, 2, 5, 4, 5, 2, 3]))
+print(solution(2, [1, 1, 1, 1, 2, 2, 2, 3]))
