6709b08cb5f095c205676f5bddc8737e7c327e0e
syl0702 <navyblue794@gmail.com>
2024-07-26 22:48:17 +0900

    studying Counter

--- programmers/귤고르기/sol.py
+++ programmers/귤고르기/sol.py
@@ -1,9 +1,10 @@
 from collections import Counter
 def solution(k, tangerine):
     answer = 0
+    # dictionary 형태로 각 숫자가 몇개 나오는지 파악하기
     tangs = Counter(tangerine)
     print(tangs)
-    
+    print(tangs.most_common(1))
     return answer
 
 print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
