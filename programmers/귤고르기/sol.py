313c06cf7b2802d8d3651a97b968503eab11be5c
syl0702 <navyblue794@gmail.com>
2024-07-29 16:42:08 +0900

    stilll

--- programmers/귤고르기/sol.py
+++ programmers/귤고르기/sol.py
@@ -1,11 +1,25 @@
 from collections import Counter
 def solution(k, tangerine):
     answer = 0
+    value_s = 0
     # dictionary 형태로 각 숫자가 몇개 나오는지 파악하기
     tangs = Counter(tangerine)
-    print(tangs)
-    print(tangs.most_common(1))
-    print(tangs.elements)
+    # print(tangs)
+    # print(tangs.most_common(1))
+    # print(tangs.elements())
+    # print(tangs.items())
+    # print(tangs.keys())
+    # print(tangs.values())
+    sorted_t = tangs.most_common()
+    print(sorted_t)
+    for key, value in sorted_t.items():
+        if value < k:
+            answer += 1
+            value_s += value
+            continue
+        else:
+            answer == 1
+
     return answer
 
 print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
