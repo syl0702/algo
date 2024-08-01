2922df3b64520e5c6910f2653e3a86d4bb449cd0
syl0702 <navyblue794@gmail.com>
2024-08-01 14:56:44 +0900

    sort

--- programmers/연속부분수열합의개수/sol.py
+++ programmers/연속부분수열합의개수/sol.py
@@ -1,10 +1,11 @@
 def solution(elements):
     answer = 0
-    prefix_sum = [0]*len(elements) #누적합을 담을 배열
-    prefix_sum[0] = elements[0]
-    for i in range(1, len(elements)):
-        prefix_sum[i] = prefix_sum[i-1] + elements[i]
-        
+    # prefix_sum = [0]*len(elements) #누적합을 담을 배열
+    # prefix_sum[0] = elements[0]
+    # for i in range(1, len(elements)):
+    #     prefix_sum[i] = prefix_sum[i-1] + elements[i]
+    elements.sort()
+    print(elements) 
     return answer
 
-print([7, 9, 1, 1, 4])
+print(solution([7, 9, 1, 1, 4]))
