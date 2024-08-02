5fa34720bd4e941b7487ee10f72a18e7f92c6e02
syl0702 <navyblue794@gmail.com>
2024-08-02 16:58:23 +0900

    two pointer algorithm trial

--- programmers/연속부분수열합의개수/sol.py
+++ programmers/연속부분수열합의개수/sol.py
@@ -5,11 +5,22 @@ def solution(elements):
     # for i in range(1, len(elements)):
     #     prefix_sum[i] = prefix_sum[i-1] + elements[i]
     # elements.sort()
-    cache = [0] * len(elements)
-    cache[0] = elements[0]
+    # cache = [0] * len(elements)
+    # cache[0] = elements[0]
 
     # 투포인터 알고리즘 공부해보자
-    print(elements) 
+    cnt = 0
+    interval_sum = 0
+    end = 0
+    M = sum(elements)
+    for start in range(len(elements)):
+        while interval_sum < M and end < len(elements):
+            interval_sum += elements[end]
+            end += 1
+        if interval_sum == M:
+            cnt+= 1
+        interval_sum -= elements[start]
+    print(cnt)
     return answer
 
 print(solution([7, 9, 1, 1, 4]))
