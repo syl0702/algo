db78dd08de7e606637ca57c2d5f2483cb6d4a963
syl0702 <navyblue794@gmail.com>
2024-08-09 15:30:46 +0900

    condition added

--- programmers/괄호회전하기/sol.py
+++ programmers/괄호회전하기/sol.py
@@ -4,7 +4,7 @@ def solution(s):
     pairs = {'[': ']', '{': '}', '(': ')'}
     for i in range(len(s)):
         s1 = s[i:] + s[:i]
-        if temp[-1] in pairs and s[0] == pairs[temp[-1]]:
+        if temp and temp[-1] in pairs and s[0] == pairs[temp[-1]]:
             temp.pop()
             print(s1)
         else:
