d4758f63aaf90bc4327d0df09369bc6d6c8f94ab
syl0702 <navyblue794@gmail.com>
2024-08-13 19:29:42 +0900

    solved! but need review

--- programmers/괄호회전하기/sol.py
+++ programmers/괄호회전하기/sol.py
@@ -5,13 +5,23 @@ def solution(s):
     for i in range(len(s)):
         # 이어 붙이기
         s1 = s[i:] + s[:i]
-        if temp and temp[-1] in pairs and s[0] == pairs[temp[-1]]:
-            temp.pop()
-            print(s1)
-            answer+=1
-        else:
-            temp.append(s[0])
-            continue
+        while True:
+            length = len(s1)
+            s1 = s1.replace("()", "")
+            s1 = s1.replace("[]", "")
+            s1 = s1.replace("{}", "")
+            if len(s1) == 0:
+                answer += 1
+                break
+            if length == len(s1): # 소거 작업 후에도 길이 변화가 없는 경우
+                break
+        # if temp and temp[-1] in pairs and s[0] == pairs[temp[-1]]:
+        #     temp.pop()
+        #     print(s1)
+        #     answer+=1
+        # else:
+        #     temp.append(s[0])
+        #     continue
     return answer
 
 print(solution("[](){}"))
