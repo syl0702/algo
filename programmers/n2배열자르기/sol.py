be11ec04d1152c9ee7f1a299ae01291963d79ba2
syl0702 <navyblue794@gmail.com>
2024-08-20 19:42:11 +0900

    confused

--- programmers/n2배열자르기/sol.py
+++ programmers/n2배열자르기/sol.py
@@ -3,7 +3,10 @@ def solution(n, left, right):
     array = [[0 for _ in range(n)] for _ in range(n)]
     print(array)
     for i in range(1, n):
-        print(i)
+        for j in range(n):
+            for k in range(n):
+                array[j][k] == i
+        print(array)
     return answer
 
 print(solution(3, 2, 5))
