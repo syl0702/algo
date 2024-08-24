edea501ad44165729767fc4927aff33aac29941f
syl0702 <navyblue794@gmail.com>
2024-08-24 23:55:23 +0900

    put numbers in array

--- programmers/n2배열자르기/sol.py
+++ programmers/n2배열자르기/sol.py
@@ -2,6 +2,7 @@ def solution(n, left, right):
     answer = []
     array = [[0 for _ in range(n)] for _ in range(n)]
     # print(array)
+    # 0만 들어간 곳에 숫자 넣기
     for i in range(1, n+1):
         for j in range(n+1):
             for k in range(n+1):
